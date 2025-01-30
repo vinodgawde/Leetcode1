0d831761f7152b4cce5bb165850de6b40acb84b4
Vinod Gawde <47134554+vinodgawde@users.noreply.github.com>
2025-01-31 02:49:48 +0530

    Time: 8 ms (33.04%), Space: 25.7 MB (17.62%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -5,14 +5,14 @@ class Solution:
         """
         n=len(nums)
         k=k%n
-        def reverse(start: int, end: int):
-            while start < end:
-                nums[start], nums[end] = nums[end], nums[start]
-                start += 1
-                end -= 1
+        def reverse(start,end):
+            while start<end:
+                nums[start],nums[end]=nums[end],nums[start]
+                start+=1
+                end-=1
         
-        reverse(0, n - 1)
-        reverse(0, k - 1)
-        reverse(k, n - 1)
-
-        
+        reverse(0, n-1)
+        # Reverse the first k elements
+        reverse(0, k-1)
+        # Reverse the remaining n-k elements
+        reverse(k, n-1)
