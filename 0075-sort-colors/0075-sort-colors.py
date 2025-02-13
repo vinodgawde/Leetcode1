4f7c70d3162d4f068601f59b44490bda9570a07c
Vinod Gawde <47134554+vinodgawde@users.noreply.github.com>
2025-02-13 22:33:21 +0530

    Time: 0 ms (100%), Space: 17.7 MB (51.11%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,19 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        count1=0
-        count2=0
-        count3=0
-        for i in range(len(nums)):
-            if nums[i]==0:
-                count1+=1
-            if nums[i]==1:
-                count2+=1
-            if nums[i]==2:
-                count3+=1
-        for i in range(count1):
-            nums[i]=0
-        for i in range(count1,count1+count2):
-            nums[i]=1
-        for i in range(count1+count2,count1+count2+count3):
-            nums[i]=2
+        n=len(nums)
+        low=0
+        mid=0
+        high=n-1
+        while (mid<=high):
+            if nums[mid] == 0:
+                nums[low],nums[mid]=nums[mid],nums[low]
+                low+=1
+                mid+=1
+            elif nums[mid] == 1:
+                mid+=1
+            else:
+                nums[mid],nums[high]=nums[high],nums[mid]
+                high-=1
