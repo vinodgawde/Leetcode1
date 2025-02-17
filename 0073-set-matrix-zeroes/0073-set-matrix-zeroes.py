8084fc59c96202ccd28cd0c081514cc7161b2006
Vinod Gawde <47134554+vinodgawde@users.noreply.github.com>
2025-02-18 02:23:02 +0530

    Time: 3 ms (71.6%), Space: 18.2 MB (99.57%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -2,55 +2,27 @@ class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         n = len(matrix)
         m = len(matrix[0])
-        row = [0] * n  # row array
-        col = [0] * m  # col array
 
-        # Traverse the matrix:
+        col0 = 1
         for i in range(n):
             for j in range(m):
                 if matrix[i][j] == 0:
-                    # mark ith index of row wih 1:
-                    row[i] = 1
+                    matrix[i][0] = 0
+                    if j != 0:
+                        matrix[0][j] = 0
+                    else:
+                        col0 = 0
+        for i in range(1,n):
+            for j in range(1,m):
+                if matrix[i][j] != 0:
+                    if matrix[i][0] == 0 or matrix[0][j] == 0:
+                        matrix[i][j] = 0
 
-                    # mark jth index of col wih 1:
-                    col[j] = 1
-
-        # Finally, mark all (i, j) as 0
-        # if row[i] or col[j] is marked with 1.
-        for i in range(n):
+        if matrix[0][0] == 0:
             for j in range(m):
-                if row[i] or col[j]:
-                    matrix[i][j] = 0
+                matrix[0][j] = 0
+        if col0 == 0:
+            for i in range(n):
+                matrix[i][0] = 0
 
         return matrix
-
-
-
-
-    # def markRow(self, matrix: List[List[int]], i: int):
-    #     for j in range(len(matrix[0])):
-    #         if matrix[i][j] != 0:
-    #             matrix[i][j] = -1
-
-    # def markCol(self, matrix: List[List[int]], j: int):
-    #     for i in range(len(matrix)):
-    #         if matrix[i][j] != 0:
-    #             matrix[i][j] = -1
-
-    # def setZeroes(self, matrix: List[List[int]]) -> None:
-    #     """
-    #     Do not return anything, modify matrix in-place instead.
-    #     """
-    #     rows = len(matrix)
-    #     cols = len(matrix[0])
-
-    #     for i in range(rows):
-    #         for j in range(cols):
-    #             if matrix[i][j] == 0:
-    #                 self.markRow(matrix, i)
-    #                 self.markCol(matrix, j)
-                
-    #     for i in range(rows):
-    #         for j in range(cols):
-    #             if matrix[i][j] == -1:
-    #                 matrix[i][j] = 0
