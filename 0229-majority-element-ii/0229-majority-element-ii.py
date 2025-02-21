f8ab5ab1b9fbd379080db3e8c7590763c00ad65b
Vinod Gawde <47134554+vinodgawde@users.noreply.github.com>
2025-02-21 21:45:51 +0530

    Time: 8 ms (14.06%), Space: 19.1 MB (41.8%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,15 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         n = len(nums)
+        hashM={}
         ans = []
+        count = 0
         for i in range(n):
-            if len(ans)==0 or nums[i] not in ans:
-                count = 0
-                for j in range(n):
-                    if nums[i]==nums[j]:
-                        count+=1
-                if count > n//3:
-                    ans.append(nums[i])
-            if len(ans) == 2:  
-                break
+            hashM[nums[i]] = hashM.get(nums[i],0)+1
+            if hashM[nums[i]] == (n//3)+1:
+                ans.append(nums[i])
+
         return ans
