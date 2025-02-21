2b46fbbd393a0647c599741d0f1d92cc4c4ed9ad
Vinod Gawde <47134554+vinodgawde@users.noreply.github.com>
2025-02-21 23:49:09 +0530

    Time: 7 ms (31.84%), Space: 19.2 MB (14.3%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,12 +1,36 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        hashM={}
-        ans = []
-        count = 0
-        for i in range(n):
-            hashM[nums[i]] = hashM.get(nums[i],0)+1
-            if hashM[nums[i]] == (n//3)+1:
-                ans.append(nums[i])
+        count1=0
+        count2=0
+        ele1=float('-inf')
+        ele2=float('-inf')
+        for i in range(len(nums)):
+            if count1==0 and ele2 != nums[i]:
+                count1=1
+                ele1=nums[i]
+            elif count2==0 and ele1 != nums[i]:
+                count2=1
+                ele2=nums[i]
+            elif ele1 == nums[i]:
+                count1 += 1
+            elif ele2 == nums[i]:
+                count2 += 1
+            else:
+                count1-=1
+                count2-=1
+        ans=[]
+        count1=0
+        count2=0
+        for i in range(len(nums)):
+            if ele1 == nums[i]:
+                count1+=1
+            if ele2 == nums[i]:
+                count2+=1
+        
+        mini = (len(nums)//3)+1
+        if count1 >= mini:
+            ans.append(ele1)
+        if count2 >= mini:
+            ans.append(ele2)
 
         return ans
